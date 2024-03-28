54ab8241f4f14b23123ebd798d1195cef05f7636
iluxa42 <iluxa3650@gmail.com>
2024-03-28 03:22:56 +0300

    done

--- src/main/java/koschei/models/Wood3.java
+++ src/main/java/koschei/models/Wood3.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class Wood3 {
 
-    private final Rabbit4 rabbit;
+    private Rabbit4 rabbit;
 
     @Autowired
     public Wood3(Rabbit4 rabbit) {
@@ -15,6 +15,6 @@ public class Wood3 {
 
     @Override
     public String toString() {
-        return ", на дереве заяц " + rabbit.toString();
+        return "на дереве заяц, " + rabbit.toString();
     }
 }
